439f3c425187290cf2323f6792e9e4f371b22bf1
unknown <voquochoau@gmail.com>
2024-11-07 20:33:30 +0700

    load

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -6,19 +6,12 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateUserRequest extends FormRequest
 {
-<<<<<<< HEAD
    
-=======
-    /**
-     * Determine if the user is authorized to make this request.
-     */
->>>>>>> syupload
     public function authorize(): bool
     {
         return true;
     }
 
-<<<<<<< HEAD
     
     public function rules(): array 
     {
@@ -30,19 +23,6 @@ class UpdateUserRequest extends FormRequest
             'birthday' => 'required',
             'address' => 'required|max:191',
             'description' => 'required|max:191',
-=======
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            'email' => 'required|string|email|unique:users,email, '.$this->id.'|max:191',
-            'name' => 'required|string|max:50',
-            'user_catalogue_id' => 'required|integer|gt:0',
->>>>>>> syupload
         ];
     }
 
@@ -50,7 +30,6 @@ class UpdateUserRequest extends FormRequest
     public function messages(): array
     {
         return [
-<<<<<<< HEAD
             'name.required' => 'Bạn chưa nhập tên',
             'name.max' => 'Độ dài tên đã quá 50 kí tự',
             'name.string' => 'Tên phải là dạng kí tự',
@@ -68,16 +47,6 @@ class UpdateUserRequest extends FormRequest
             'address.required' => 'Bạn chưa nhập rõ địa chỉ.',
             'address.max' => 'Độ dài phần địa chỉ đã quá 191 kí tự',
             
-=======
-            'email.required' => 'Vui lòng nhập email.',
-            'email.email' => 'Nhập email chưa đúng. Ví dụ: abc@gmail',
-            'email.unique' => 'Email đã tồn tại. Hãy chọn email khác!',
-            'email.max' => 'Độ dài của email tối đa 191 ký tự',
-            'name.required' => 'Vui lòng nhập họ tên.',
-            'name.string' => 'Họ tên phải là dạng ký tự',
-            'name.max' => 'Độ dài của họ tên tối đa 50 ký tự',
-            'user_catalogue_id.gt' => 'Bạn chưa chọn nhóm thành viên',
->>>>>>> syupload
         ];
     }
 }
