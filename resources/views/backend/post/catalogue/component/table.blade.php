8eb4a7c4b78538ea0cb18933be4d072571cd718a
linhlinh <linhhien434@gmail.com>
2024-11-19 23:26:35 +0700

    translate post by Linh

--- resources/views/backend/post/catalogue/component/table.blade.php
+++ resources/views/backend/post/catalogue/component/table.blade.php
@@ -2,24 +2,21 @@
     <div class="table-responsive">
         <table class="table  mb-0 table-centered">
             <thead class="table-light">
-            <tr>
-                <th style="width: 16px;">
-                    <input type="checkbox" class="form-check-input checkBoxItem" id="checkAll">                                                    
-                </th>
-                <th>Tên nhóm</th>
-
-                @foreach($languages as $language)
-                            @if(session('app_locale') === $language->canonical)
-                                @continue; 
-                             @endif
-                    <th class="text-center"><span class="image img-scaledown laguange-flag"><img src="{{ $language->image}}" alt="" style="width:40px;"></span></th>
-                @endforeach
-
-                <th>Tình trạng</th>
-                <th class="text-end">Thao tác</th>
-            </tr>
+                <tr>
+                    <th style="width: 16px;">
+                        <input type="checkbox" class="form-check-input checkBoxItem" id="checkAll">                                                    
+                    </th>
+                    <th>Tên nhóm</th>
+                    @foreach($languages as $language)
+                        @if(session('app_locale') === $language->canonical)
+                            @continue; 
+                        @endif
+                        <th class="text-center"><span class="image img-scaledown laguange-flag"><img src="{{ $language->image}}" alt="" style="width:40px;"></span></th>
+                    @endforeach
+                    <th>Tình trạng</th>
+                    <th class="text-end">Thao tác</th>
+                </tr>
             </thead>
-            {{-- @php dd(session('app_locale')) @endphp --}}
             <tbody>
                 @foreach ($postCatalogues as $postCatalogue)
                     <tr>
@@ -28,7 +25,7 @@
                         </th>
                         <td>{{ str_repeat('|----', (($postCatalogue-> level >0) ?($postCatalogue-> level - 1) : 0)).$postCatalogue-> name }}</td>
                         
-                        @include('backend.dashboard.component.languageTd', ['model' => $postCatalogue, 'modeling' => 'PostCatalogue']);
+                        @include('backend.dashboard.component.languageTd', ['model' => $postCatalogue, 'modeling' => 'PostCatalogue'])
                         <td>
                             <div class="form-switch">
                                 <input class="form-check-input status js-switch-{{ $postCatalogue-> id }}" 
@@ -38,7 +35,7 @@
                                     value="{{ $postCatalogue->publish }}"  
                                     data-modeId="{{ $postCatalogue->id }}"
                                     {{ $postCatalogue->publish == 2 ? 'checked' : '' }} 
-                                    >
+                                >
                             </div>
                         </td>
                         <td class="text-end">                                                        
