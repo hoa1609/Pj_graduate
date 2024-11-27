afd4e6f3f6ea3baf2cc0d2c53320de1f19b75573
unknown <voquochoau@gmail.com>
2024-11-27 17:59:40 +0700

    done order

--- resources/views/backend/dashboard/component/nav.blade.php
+++ resources/views/backend/dashboard/component/nav.blade.php
@@ -10,14 +10,14 @@
                     </button>
                 </li> 
                 <li class="mx-3 welcome-text">
-                    <h4 class="mb-0 fw-bold text-truncate">Good Morning, James!</h4>
+                    {{-- <h4 class="mb-0 fw-bold text-truncate">Good Morning, James!</h4> --}}
                     <!-- <h6 class="mb-0 fw-normal text-muted text-truncate fs-14">Here's your overview this week.</h6> -->
                 </li>                   
             </ul>
             <ul class="topbar-item list-unstyled d-inline-flex align-items-center mb-0">
                 <li class="hide-phone app-search">
                     <form role="search" action="#" method="get">
-                        <input type="search" name="search" class="form-control top-search mb-0" placeholder="Search here...">
+                        <input type="search" name="search" class="form-control top-search mb-0" placeholder="Tìm kiếm...">
                         <button type="submit"><i class="iconoir-search"></i></button>
                     </form>
                 </li>     
